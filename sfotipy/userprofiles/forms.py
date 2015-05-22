43114c20eef8ee67f2f51ebb01502194bbd8713f
Juliette Cetina <juliethcetina@gmail.com>
2015-05-22 15:28:56 -0500

    Se creo vista para loguear usuarios, usando backends

--- sfotipy/userprofiles/forms.py
+++ sfotipy/userprofiles/forms.py
@@ -2,6 +2,8 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User 
 
+from django.contrib.auth import authenticate
+
 class UserCreationEmailForm(UserCreationForm):
 	email = forms.EmailField()
 
@@ -9,3 +11,26 @@ class UserCreationEmailForm(UserCreationForm):
 		model = User
 		fields = ('username', 'email')
 		
+class EmailAuthenticationForm(forms.Form):
+	email = forms.EmailField()
+	password = forms.CharField(label='Password', widget=forms.PasswordInput)
+
+	def __init__(self, *args, **kwargs):
+		self.user_cache = None
+		super(EmailAuthenticationForm, self).__init__(*args, **kwargs)
+
+	def clean(self):
+		email = self.cleaned_data.get('email')
+		password = self.cleaned_data.get('password')
+
+		self.user_cache = authenticate(email=email, password=password)
+
+		if self.user_cache is None:
+			raise forms.ValidationError('Usuario incorrecto.')
+		elif not self.user_cache.is_active:
+			raise forms.ValidationError('Usuario inactivo.')
+
+		return self.cleaned_data
+
+	def get_user(self):
+		return self.user_cache
--- sfotipy/userprofiles/views.py
+++ sfotipy/userprofiles/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from .forms import UserCreationEmailForm
+from .forms import UserCreationEmailForm, EmailAuthenticationForm
+
+from django.contrib.auth import login
 
 def signup(request):
 	form = UserCreationEmailForm(request.POST or None)
@@ -8,3 +10,11 @@ def signup(request):
 		form.save()
 
 	return render(request, 'signup.html', {'form': form})
+
+def signin(request):
+	form = EmailAuthenticationForm(request.POST or None)
+
+	if form.is_valid():
+		login(request, form.get_user())
+
+	return render(request, 'signin.html', {'form': form})
