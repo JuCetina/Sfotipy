9f7485dd8306724cf8de953dfcec9450f47e5c85
Juliette Cetina <juliethcetina@gmail.com>
2015-05-21 19:21:26 -0500

    Se envio la informacion de un track como tipo json

--- sfotipy/tracks/views.py
+++ sfotipy/tracks/views.py
@@ -1,3 +1,4 @@
+import json
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404
 
@@ -7,5 +8,19 @@ def track_view(request, title):
 
 	track = get_object_or_404(Track, title=title)
 
-	return render(request, 'track.html', {'track': track})
+	data = {
+		'title': track.title,
+		'order': track.order,
+		'album': track.album.title,
+		'artist': {
+			'name': track.artist.first_name,
+			'bio': track.artist.biography
+		}
+	}
+
+	json_data = json.dumps(data)
+
+	return HttpResponse(json_data, content_type='application/json')
+
+	#return render(request, 'track.html', {'track': track})
 
