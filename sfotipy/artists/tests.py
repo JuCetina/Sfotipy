9b660efe5777f6e27847622a5eef4a68a0efb56d
Juliette Cetina <juliethcetina@gmail.com>
2015-06-20 17:54:20 -0500

    Se realizo una prueba basica en artists

--- sfotipy/artists/tests.py
+++ sfotipy/artists/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
 
 # Create your tests here.
+from .models import Artist
+
+class TestArtists(TestCase):
+
+	def setUp(self):
+		self.artist = Artist.objects.create(first_name='david', last_name='bowie')
+
+	def test_existe_vista(self):
+		res = self.client.get('/artists/%d/' % self.artist.id)
+		self.assertEqual(res.status_code, 200)
+		self.assertTrue('david' in res.content)
+		
