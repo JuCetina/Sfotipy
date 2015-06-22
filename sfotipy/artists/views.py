9df5c1b34adf38cf8ee8b8488c84e483e72b2ba6
Juliette Cetina <juliethcetina@gmail.com>
2015-06-22 18:01:41 -0500

    Se agrego la paginacion, los campos en artists

--- sfotipy/artists/views.py
+++ sfotipy/artists/views.py
@@ -17,3 +17,5 @@ from .serializers import ArtistSerializer
 class ArtistViewSet(viewsets.ModelViewSet):
 	model = Artist
 	serializer_class = ArtistSerializer
+	filter_fields = ('id', )
+	paginate_by = 2
