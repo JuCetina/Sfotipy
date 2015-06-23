60d2f8bbc8df7ead2d6d7fc4adbc6abf809daea9
Juliette Cetina <juliethcetina@gmail.com>
2015-06-23 00:53:28 -0500

    Caching low level

--- sfotipy/tracks/views.py
+++ sfotipy/tracks/views.py
@@ -2,22 +2,27 @@ import json
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404
 
+import time
+from django.core.cache import cache
+
 from .models import Track
 
 def track_view(request, title):
 
 	track = get_object_or_404(Track, title=title)
-	
-	data = {
-		'title': track.title,
-		'order': track.order,
-		'album': track.album.title,
-		'artist': {
-			'name': track.artist.first_name,
-			'bio': track.artist.biography
+	data = cache.get('data_%s', title)
+	if data is None:
+		data = {
+			'title': track.title,
+			'order': track.order,
+			'album': track.album.title,
+			'artist': {
+				'name': track.artist.first_name,
+				'bio': track.artist.biography
+			}
 		}
-	}
-
+		time.sleep(5)
+		cache.set('data_%s' % title, data)
 	#json_data = json.dumps(data) #Convierte el diccionario a json
 
 	#return HttpResponse(json_data, content_type='application/json')
