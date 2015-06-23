5b469177de7efd10e7d13dc25be620c834b86f1c
Juliette Cetina <juliethcetina@gmail.com>
2015-06-23 00:58:02 -0500

    Caching per view

--- sfotipy/tracks/views.py
+++ sfotipy/tracks/views.py
@@ -5,24 +5,28 @@ from django.http import HttpResponse, Http404
 import time
 from django.core.cache import cache
 
+from django.views.decorators.cache import cache_page
+
 from .models import Track
+
+@cache_page(60)
 
 def track_view(request, title):
 
 	track = get_object_or_404(Track, title=title)
-	data = cache.get('data_%s', title)
-	if data is None:
-		data = {
-			'title': track.title,
-			'order': track.order,
-			'album': track.album.title,
-			'artist': {
-				'name': track.artist.first_name,
-				'bio': track.artist.biography
-			}
+	#data = cache.get('data_%s', title)
+	#if data is None:
+	data = {
+		'title': track.title,
+		'order': track.order,
+		'album': track.album.title,
+		'artist': {
+			'name': track.artist.first_name,
+			'bio': track.artist.biography
 		}
-		time.sleep(5)
-		cache.set('data_%s' % title, data)
+	}
+		#time.sleep(5)
+		#cache.set('data_%s' % title, data)
 	#json_data = json.dumps(data) #Convierte el diccionario a json
 
 	#return HttpResponse(json_data, content_type='application/json')
